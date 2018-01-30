eb5586ea6852649905c42b248e784ae5dfd9a54e
oliverjam <oliverjamesphillips@gmail.com>
2018-01-30 22:08:29 +0000

    Remove unused app state

--- components/app.js
+++ components/app.js
@@ -2,22 +2,11 @@ import React from 'react';
 import Tamagotchi from './tamagotchi';
 
 class App extends React.Component {
-  state = {
-    name: '',
-    login: '',
-    img: '',
-    followers: 0,
-    fetched: false,
-  };
-  updateData = ({ name, login, img, followers, fetched }) => {
-    this.setState({ name, login, img, followers, fetched });
-  };
   render() {
-    const { name, login, img, fetched } = this.state;
     return (
       <div className="container">
         <h1>Github Tamagotchi 🐙</h1>
-        <Tamagotchi name={name || login} img={img} start={fetched} />
+        <Tamagotchi />
         <section>
           <h2>Key:</h2>
           <ul style={{ marginTop: '0.5em' }}>
