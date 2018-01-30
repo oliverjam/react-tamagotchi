9add28253b2c48c09a2f702389fd41ee85261d70
oliverjam <oliverjamesphillips@gmail.com>
2018-01-30 22:13:59 +0000

    Add explanatory comments to main component

--- components/tamagotchi/index.js
+++ components/tamagotchi/index.js
@@ -11,16 +11,21 @@ class Tamagotchi extends React.Component {
     motivation: 0,
     burnout: false,
   };
+  // passed to child as a prop so it can update the state here
+  // see https://reactjs.org/docs/lifting-state-up.html
   updateData = data => {
     this.setState({ data, fetched: true });
   };
   startTimer = () => {
     this.setState({ motivation: 5 });
+    // start a timer to reduce motivation by 1 every 5s
     this.interval = setInterval(() => {
       this.setState(
         prevState => {
           return { motivation: prevState.motivation - 1 };
         },
+        // after our state update check if we're out of motivation
+        // if so we end the game and stop the timer
         () => {
           if (this.state.motivation < 1) {
             this.setState({ burnout: true, running: false, motivation: 5 });
@@ -31,6 +36,7 @@ class Tamagotchi extends React.Component {
     }, DEC_INTERVAL);
   };
   incrementMotivation = inc => {
+    // ensure we don't go higher than 5
     if (this.state.motivation + inc > 5) {
       return this.setState({ motivation: 5 });
     }
@@ -38,14 +44,19 @@ class Tamagotchi extends React.Component {
       return { motivation: prevState.motivation + inc };
     });
   };
+  // called every time we receive props or state changes
   componentDidUpdate() {
+    // if the game is 'over' then don't start a timer
     if (this.state.burnout) return;
+    // if we have data and aren't currently running then start
     if (this.state.fetched && !this.state.running) {
       this.setState({ running: true });
       this.startTimer();
     }
   }
+  // called when React removes the component
   componentWillUnmount() {
+    // stops the timer from continuing to run
     clearInterval(this.interval);
   }
   render() {
