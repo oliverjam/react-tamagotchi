303df110a9e8f0f01f68c8e83d58d9603fa2c0e9
oliverjam <oliverjamesphillips@gmail.com>
2020-05-08 15:34:30 +0100

    Remove unnecessary key list

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Tamagotchi from './components/tamagotchi';
-import './index.css';
+import React from "react";
+import { render } from "react-dom";
+import Tamagotchi from "./components/tamagotchi";
+import "./index.css";
 
 const App = () => (
   <div className="container">
-    <h1>Github Tamagotchi 🐙</h1>
+    <h1>Github Tamagotchi</h1>
     <Tamagotchi />
-    <section>
-      <h2>Key:</h2>
-      <ul style={{ marginTop: '0.5em' }}>
-        <li>💜: Compliment their code</li>
-        <li>🐶: Send them a cute gif</li>
-        <li>⭐️: Star their side-project</li>
-      </ul>
-    </section>
   </div>
 );
 
 render(
   <App />,
-  document.getElementById('root') // eslint-disable-line no-undef
+  document.getElementById("root") // eslint-disable-line no-undef
 );
