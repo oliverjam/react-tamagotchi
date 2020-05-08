c4ec9658895c686dff1b8fe1ade7c2cd860e97f5
oliverjam <oliverjamesphillips@gmail.com>
2020-05-08 16:44:50 +0100

    Add labels to images

--- src/components/motivationBar.js
+++ src/components/motivationBar.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import heartIcon from '../assets/heart.svg';
+import React from "react";
+import heartIcon from "../assets/heart.svg";
 
 const MotivationBar = ({ health }) => {
   const hearts = Array.from({ length: health }, (item, i) => (
-    <img src={heartIcon} className="tamagotchi__heart" key={i} />
+    <img src={heartIcon} className="tamagotchi__heart" key={i} alt="heart" />
   ));
   return (
-    <div style={{ display: 'flex' }}>
+    <div style={{ display: "flex" }}>
       <span>Motivation:</span>
       <span className="tamagotchi__hearts">{hearts}</span>
     </div>
--- src/components/screen.js
+++ src/components/screen.js
@@ -1,39 +1,54 @@
-import React from 'react';
-import Search from './search';
-import MotivationBar from './motivationBar';
-import skullIcon from '../assets/skull.svg';
+import React from "react";
+import Search from "./search";
+import MotivationBar from "./motivationBar";
+import skullIcon from "../assets/skull.svg";
 
 const ErrorView = () => (
   <div className="tamagotchi__screen">
     <div>There’s been a terrible mistake</div>
-    <img className="tamagotchi__img" src={skullIcon} alt="A skull and crossbones" />
+    <img
+      className="tamagotchi__img"
+      src={skullIcon}
+      alt="A skull and crossbones"
+    />
     <div>Please try again :)</div>
   </div>
-)
+);
 
 const Screen = ({ error, name, img, burnout, motivation, updateData }) => {
   // render a different view if there's a fetch error
-  if (error) return <ErrorView />
+  if (error) return <ErrorView />;
   return (
     <div className="tamagotchi__screen">
-      {name
-        ? <div className="tamagotchi__name">{name}</div>
-        : <Search updateParentState={updateData} />
-      }
-      {burnout
-        ? <div className="tamagotchi__img">😵</div>
-        : <img className="tamagotchi__img" src={img} />
-      }
-      { // we use '!!' to force motivation to be a boolean here
+      {name ? (
+        <div className="tamagotchi__name">{name}</div>
+      ) : (
+        <Search updateParentState={updateData} />
+      )}
+      {burnout ? (
+        <span
+          className="tamagotchi__img"
+          role="img"
+          aria-label="They've burnt out!"
+        >
+          😵
+        </span>
+      ) : (
+        <img className="tamagotchi__img" src={img} alt="Their GitHub profile" />
+      )}
+      {
+        // we use '!!' to force motivation to be a boolean here
         // otherwise we get '0' rendered as React will 'toString' it
         !!motivation && (
-        <div className="tamagotchi__motivation">
-          {burnout
-            ? `${name} has burnt out!`
-            : <MotivationBar health={motivation} />
-          }
-        </div>
-      )}
+          <div className="tamagotchi__motivation">
+            {burnout ? (
+              `${name} has burnt out!`
+            ) : (
+              <MotivationBar health={motivation} />
+            )}
+          </div>
+        )
+      }
     </div>
   );
 };
