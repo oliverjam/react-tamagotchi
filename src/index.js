e6824322b5317c9390018763baab92d250f44102
oliverjam <oliverjamesphillips@gmail.com>
2020-05-08 18:36:48 +0100

    Simplify layout

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import { render } from "react-dom";
 import Tamagotchi from "./components/tamagotchi";
 import "./index.css";
 
-const App = () => (
-  <div className="container">
-    <h1>Github Tamagotchi</h1>
-    <Tamagotchi />
-  </div>
-);
-
 render(
-  <App />,
+  <Tamagotchi />,
   document.getElementById("root") // eslint-disable-line no-undef
 );
